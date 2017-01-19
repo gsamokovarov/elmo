bc20a9b7a0ef7a46ef5a72f28752c47782b4a81d
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-19 10:48:51 +0200

    Stub-out the memory

---
+++ src/Elmo/Memory.elm
@@ -0,0 +1,20 @@
+module Elm.Memory exposing (..)
+
+
+type alias Memory =
+    Array Int
+
+
+initialize : Memory
+initialize =
+    Array.repeat 2048 0
+
+
+read : Int -> Memory
+read address memory =
+    Array.get address memory
+
+
+write : Int -> Int -> Memory
+write address value memory =
+    Array.set address value memory
