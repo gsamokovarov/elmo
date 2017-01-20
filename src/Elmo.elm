050261bf85f6c04ba9348187d8b67578eeb2ee11
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-20 12:36:09 +0200

    Link-up the NES development wiki
    So much documentation!

--- src/Elmo.elm
+++ src/Elmo.elm
@@ -2,4 +2,7 @@ module Elmo exposing (..)
 
 {-| Nintendo Entertainment System (NES) emulator written in Elm
     for the web.
+
+    Check out http://wiki.nesdev.com/w/index.php/NES_reference_guide for
+    documentation.
 -}
