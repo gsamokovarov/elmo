501a4489011907586895fd9da47df997477fa2fe
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-01-31 10:59:33 +0200

    Use a deconstruction shortcut instead of let

--- src/Elmo/Core.elm
+++ src/Elmo/Core.elm
@@ -36,18 +36,14 @@ type alias System =
 
 
 step : System -> System
-step system =
-    let
-        { cpu, memory } =
-            system
-    in
-        if cpu.stall > 0 then
-            { system | cpu = { cpu | stall = cpu.stall - 1 } }
-        else
-            memory
-                |> Memory.read cpu.pc
-                |> Opcode.dispatch
-                |> process system
+step ({ cpu, memory } as system) =
+    if cpu.stall > 0 then
+        { system | cpu = { cpu | stall = cpu.stall - 1 } }
+    else
+        memory
+            |> Memory.read cpu.pc
+            |> Opcode.dispatch
+            |> process system
 
 
 process : System -> Instruction -> System
@@ -77,28 +73,20 @@ nop system =
 https://wiki.nesdev.com/w/index.php/Stack
 -}
 stackPush : System -> Int -> System
-stackPush system value =
-    let
-        { cpu, memory } =
-            system
-    in
-        -- TODO(genadi): The stack is between 0x0100-0x01FF, make sure to keep it
-        -- around that length and handle overflow and underflows correctly.
-        { system
-            | cpu = { cpu | sp = cpu.sp - 1 }
-            , memory = memory |> Memory.write cpu.sp value
-        }
+stackPush ({ cpu, memory } as system) value =
+    -- TODO(genadi): The stack is between 0x0100-0x01FF, make sure to keep it
+    -- around that length and handle overflow and underflows correctly.
+    { system
+        | cpu = { cpu | sp = cpu.sp - 1 }
+        , memory = memory |> Memory.write cpu.sp value
+    }
 
 
 {-| For a detailed look on how the 6502 stack works, go to:
 https://wiki.nesdev.com/w/index.php/Stack
 -}
 stackPull : System -> ( System, Int )
-stackPull system =
-    let
-        { cpu, memory } =
-            system
-    in
-        ( { system | cpu = { cpu | sp = cpu.sp + 1 } }
-        , Memory.read (cpu.sp + 1) memory
-        )
+stackPull ({ cpu, memory } as system) =
+    ( { system | cpu = { cpu | sp = cpu.sp + 1 } }
+    , Memory.read (cpu.sp + 1) memory
+    )
