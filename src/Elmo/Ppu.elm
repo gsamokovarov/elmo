30d6742cd064e86ec58004d47e40ff7e976726f9
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-02-12 18:02:38 +0200

    Introduce the first baby steps of the PPU

---
+++ src/Elmo/Ppu.elm
@@ -0,0 +1,24 @@
+module Elmo.Ppu exposing (..)
+
+import Elmo.Types exposing (Ppu)
+
+
+init : Ppu
+init =
+    { ppuctrl = 0
+    , ppumask = 0
+    , ppustatus = 0
+    , oamaddr = 0
+    , oamdata = 0
+    , ppuscroll = 0
+    , ppuaddr = 0
+    , ppudata = 0
+    , oamdma = 0
+    , v = 0
+    , t = 0
+    , x = 0
+    , w = 0
+    , cycle = 0
+    , frame = 0
+    , scanLine = 0
+    }
