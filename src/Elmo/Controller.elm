c2e36201e2bfb100ce3bfdef454a6a16e9d5c324
Genadi Samokovarov <gsamokovarov@gmail.com>
2017-02-14 18:36:16 +0200

    Imaginative controller memory handling

--- src/Elmo/Controller.elm
+++ src/Elmo/Controller.elm
@@ -12,11 +12,14 @@ Button |    A      B   SELECT   START   UP    DOWN   LEFT  RIGHT
 See http://fms.komkon.org/EMUL8/NES.html#LABB
 -}
 
+import Elmo.Utils exposing ((<<<), (&&&), (|||), count)
+
 
 {-| A snapshot of a NES input controller state.
 -}
 type alias Controller =
     { buttons : Int
+    , i : Int
     , s : Int
     }
 
@@ -27,6 +30,7 @@ a regular devices and four with an extension pack.
 init : Controller
 init =
     { buttons = 0
+    , i = 0
     , s = 0
     }
 
@@ -40,6 +44,36 @@ once.
 press : Int -> Controller -> Controller
 press buttons controller =
     { controller | buttons = buttons }
+
+
+{-| Reads the buttons from controller 1 at 0x4016 or controller 2 at 0x4017.
+-}
+read : Controller -> (Controller -> Int)
+read ({ buttons, i, s } as controller) =
+    ( { controller
+        | i =
+            if (s &&& 1) == 1 then
+                0
+            else
+                i + 1
+      }
+    , count (buttons &&& (1 <<< i) == (1 <<< i))
+    )
+
+
+{-| Writes the buttons to controller 1 and controller 2. Both of them should be
+written to 0x4017.
+-}
+write : Int -> Controller -> Controller
+write value ({ i, s } as controller) =
+    { controller
+        | s = value
+        , i =
+            if (value &&& 1) == 1 then
+                0
+            else
+                i
+    }
 
 
 
